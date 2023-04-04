7061604f277c59e6618086924c6d3b1b8788ee03
shuw <shuw@nvidia.com>
2023-04-04 19:47:47 +0000

    fist commit

---
+++ TF/DenseFp8.py
@@ -0,0 +1,272 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Contains the Dense layer."""
+
+
+import tensorflow.compat.v2 as tf
+
+from keras import activations
+from keras import backend
+from keras import constraints
+from keras import initializers
+from keras import regularizers
+from keras.engine.input_spec import InputSpec
+
+# isort: off
+from tensorflow.python.util.tf_export import keras_export
+
+from tensorflow.python.framework import dtypes
+
+FAKE_E4M3 = dtypes.float8_e4m3fn
+FAKE_E5M2 = dtypes.float8_e5m2
+
+E4M3_MAX = 448.
+E5M2_MAX = 57344.
+AMAX_HIS_LEN = 16
+
+def get_fp8_max(fake_dtype):
+  if fake_dtype == FAKE_E4M3:
+    return E4M3_MAX
+  else:
+    assert fake_dtype == FAKE_E5M2
+    return E5M2_MAX
+
+def quantize(x, quantized_dtype, scale):
+  dtype_max = get_fp8_max(quantized_dtype)
+  policy = tf.keras.mixed_precision.global_policy()
+  is_mixed_policy = (
+      policy is not None and policy.compute_dtype != policy.variable_dtype
+  )
+
+  scaled_x = x / (tf.cast(scale, tf.float16) if is_mixed_policy else scale)
+  clipped_x = tf.clip_by_value(scaled_x, -dtype_max, dtype_max)
+  return tf.cast(clipped_x, quantized_dtype)
+
+def dequantize(x, wide_dtype, scale):
+  return tf.cast(x, wide_dtype) * tf.cast(scale, wide_dtype)
+
+def quantize_dequantize(x, quantized_dtype, scale):
+  orig_dtype = x.dtype
+  qx = quantize(x, quantized_dtype, scale)
+  return dequantize(qx, orig_dtype, scale)
+
+def update_scale(x, quantized_dtype, scale_var, amax_history):
+  dtype_max = get_fp8_max(quantized_dtype)
+
+  amax_current = tf.cast(tf.math.reduce_max(tf.math.abs(x)), scale_var.dtype)
+
+  amax_his_tsr = tf.tensor_scatter_nd_update(
+      tf.roll(amax_history.read_value(), 1, 0), [[0]], [amax_current])
+  amax_history.assign(tf.cast(amax_his_tsr, amax_history.dtype))
+
+  amax_temp = tf.reduce_max(amax_history, axis=0)
+  amax = tf.maximum(amax_temp, 2 ** -10)
+  scale_var.assign(tf.cast(1.1 * amax / dtype_max, scale_var.dtype))
+
+def qdq_and_update(x, dtype, scale_var, amax_history):
+  qx = quantize_dequantize(x, dtype, scale_var)
+  update_scale(x, dtype, scale_var, amax_history)
+  return qx
+
+class DenseFp8(tf.keras.layers.Dense):
+    """Just your regular densely-connected NN layer.
+    `Dense` implements the operation:
+    `output = activation(dot(input, kernel) + bias)`
+    where `activation` is the element-wise activation function
+    passed as the `activation` argument, `kernel` is a weights matrix
+    created by the layer, and `bias` is a bias vector created by the layer
+    (only applicable if `use_bias` is `True`). These are all attributes of
+    `Dense`.
+    Note: If the input to the layer has a rank greater than 2, then `Dense`
+    computes the dot product between the `inputs` and the `kernel` along the
+    last axis of the `inputs` and axis 0 of the `kernel` (using `tf.tensordot`).
+    For example, if input has dimensions `(batch_size, d0, d1)`, then we create
+    a `kernel` with shape `(d1, units)`, and the `kernel` operates along axis 2
+    of the `input`, on every sub-tensor of shape `(1, 1, d1)` (there are
+    `batch_size * d0` such sub-tensors).  The output in this case will have
+    shape `(batch_size, d0, units)`.
+    Besides, layer attributes cannot be modified after the layer has been called
+    once (except the `trainable` attribute).
+    When a popular kwarg `input_shape` is passed, then keras will create
+    an input layer to insert before the current layer. This can be treated
+    equivalent to explicitly defining an `InputLayer`.
+    Example:
+    >>> # Create a `Sequential` model and add a Dense layer as the first layer.
+    >>> model = tf.keras.models.Sequential()
+    >>> model.add(tf.keras.Input(shape=(16,)))
+    >>> model.add(tf.keras.layers.Dense(32, activation='relu'))
+    >>> # Now the model will take as input arrays of shape (None, 16)
+    >>> # and output arrays of shape (None, 32).
+    >>> # Note that after the first layer, you don't need to specify
+    >>> # the size of the input anymore:
+    >>> model.add(tf.keras.layers.Dense(32))
+    >>> model.output_shape
+    (None, 32)
+    Args:
+      units: Positive integer, dimensionality of the output space.
+      activation: Activation function to use.
+        If you don't specify anything, no activation is applied
+        (ie. "linear" activation: `a(x) = x`).
+      use_bias: Boolean, whether the layer uses a bias vector.
+      kernel_initializer: Initializer for the `kernel` weights matrix.
+      bias_initializer: Initializer for the bias vector.
+      kernel_regularizer: Regularizer function applied to
+        the `kernel` weights matrix.
+      bias_regularizer: Regularizer function applied to the bias vector.
+      activity_regularizer: Regularizer function applied to
+        the output of the layer (its "activation").
+      kernel_constraint: Constraint function applied to
+        the `kernel` weights matrix.
+      bias_constraint: Constraint function applied to the bias vector.
+    Input shape:
+      N-D tensor with shape: `(batch_size, ..., input_dim)`.
+      The most common situation would be
+      a 2D input with shape `(batch_size, input_dim)`.
+    Output shape:
+      N-D tensor with shape: `(batch_size, ..., units)`.
+      For instance, for a 2D input with shape `(batch_size, input_dim)`,
+      the output would have shape `(batch_size, units)`.
+    """
+
+    def __init__(
+        self,
+        units,
+        input_from_keras_layer=True,
+        output_to_keras_layer=True,
+        **kwargs,
+    ):
+      super().__init__(units, **kwargs)
+      self.input_from_keras_layer = input_from_keras_layer
+      self.output_to_keras_layer = output_to_keras_layer
+
+    def build(self, input_shape):
+      super().build(input_shape)
+
+      # TODO(shuw): why 0.9?
+      init32 = tf.keras.initializers.Constant(1.)
+      self.input_amax_history = self.add_weight(
+          "input_amax_history", shape=(AMAX_HIS_LEN,), initializer=init32,
+          trainable=False)
+      self.input_scale = self.add_weight(
+          "input_scale", shape=(), initializer=init32, trainable=False)
+
+      self.kernel_amax_history = self.add_weight(
+          "kernel_amax_history", shape=(AMAX_HIS_LEN,), initializer=init32,
+          trainable=False)
+      self.kernel_scale = self.add_weight(
+          "kernel_scale", shape=(), initializer=init32, trainable=False)
+
+      self.input_grad_amax_history = self.add_weight(
+          "input_grad_amax_history", shape=(AMAX_HIS_LEN,),
+          initializer=init32, trainable=False)
+      self.input_grad_scale = self.add_weight(
+          "input_grad_scale", shape=(), initializer=init32, trainable=False)
+
+      self.output_grad_amax_history = self.add_weight(
+          "output_grad_amax_history", shape=(AMAX_HIS_LEN,),
+          initializer=init32, trainable=False)
+      self.output_grad_scale = self.add_weight(
+          "output_grad_scale", shape=(),
+          initializer=init32, trainable=False)
+
+      self.built = True
+
+    @tf.custom_gradient
+    def in_qdq(self, inp):
+      """Quantize-dequantize the input and its gradient."""
+      qin = qdq_and_update(inp, FAKE_E4M3, self.input_scale,
+                           self.input_amax_history)
+
+      def grad(in_grad):
+        if self.input_from_keras_layer:
+            return in_grad
+        return qdq_and_update(in_grad, FAKE_E5M2, self.input_grad_scale,
+                              self.input_grad_amax_history)
+
+      return qin, grad
+
+    @tf.custom_gradient
+    def out_qdq(self, out):
+      """Quantize-dequantize the output and its gradient"""
+
+      def grad(out_grad):
+          if self.output_to_keras_layer:
+              return qdq_and_update(out_grad, FAKE_E5M2, self.output_grad_scale,
+                                    self.output_grad_amax_history)
+          return out_grad
+      return out, grad
+
+    @tf.custom_gradient
+    def kernel_qdq(self, kernel):
+      """Quantize-dequantize the kernel but not its gradient."""
+
+      qkernel = qdq_and_update(kernel, FAKE_E4M3, self.kernel_scale,
+                               self.kernel_amax_history)
+      if self.is_mixed_policy:
+        qkernel = tf.cast(qkernel, tf.float16)
+
+      def grad(kernel_grad, variables=None):
+        return kernel_grad
+
+      return qkernel, grad
+
+    def call(self, inputs):
+      if (isinstance(inputs, tf.RaggedTensor) or
+          isinstance(inputs, tf.SparseTensor)):
+        return super().call(inputs)
+        
+      if inputs.dtype.base_dtype != self._compute_dtype_object.base_dtype:
+        inputs = tf.cast(inputs, dtype=self._compute_dtype_object)
+
+      policy = tf.keras.mixed_precision.global_policy()
+      self.is_mixed_policy = (
+          policy is not None and policy.compute_dtype != policy.variable_dtype
+      )
+      
+      rank = inputs.shape.rank
+      if rank == 2 or rank is None:
+        outputs = tf.matmul(a=self.in_qdq(inputs),
+                            b=self.kernel_qdq(self.kernel))
+      # TODO(shuw): originally, the keras uses the tensordot. But now we use
+      # the matmul instead. Do we still care?
+      # Broadcast kernel to inputs.
+      else:
+        outputs = tf.matmul(a=self.in_qdq(inputs),
+                            b=self.kernel_qdq(self.kernel))
+        # Reshape the output back to the original ndim of the input.
+        if not tf.executing_eagerly():
+          shape = inputs.shape.as_list()
+          output_shape = shape[:-1] + [self.kernel.shape[-1]]
+          outputs.set_shape(output_shape)
+
+      if self.use_bias:
+        outputs = tf.nn.bias_add(outputs, self.bias)
+
+      if self.activation is not None:
+        outputs = self.activation(outputs)
+
+      outputs = self.out_qdq(outputs)
+
+      return outputs
+
+    def get_config(self):
+      config = super().get_config()
+      config.update(
+          {
+              "input_from_keras_layer": self.input_from_keras_layer,
+              "output_to_keras_layer": self.output_to_keras_layer,
+          }
+      )
+      return config
---
+++ TF/tf_dense.py
@@ -0,0 +1,303 @@
+import tensorflow as tf
+import argparse
+import time
+
+from tensorflow.keras import layers
+from typing import Optional
+
+dropout_rate = 0.0
+model_size_scale = 1
+from tensorflow.python.framework import dtypes
+import DenseFp8 as dense_fp8
+
+parser = argparse.ArgumentParser(description='config')
+parser.add_argument('--fp8', action='store_true', help='use_fp8')
+parser.add_argument('--train', action='store_true', help='is_train')
+parser.add_argument('--mixed', action='store_true', help='mixed_precision')
+parser.add_argument('--scale', type=int, help='model_scale', default=1)
+args = parser.parse_args()
+
+model_size_scale = args.scale
+use_fp8 = args.fp8
+is_train = args.train
+use_mixed = args.mixed
+print("DEBUG: use_fp8", use_fp8)
+print("DEBUG: is train", is_train)
+print("DEBUG: use_mixed", use_mixed)
+print("DEBUG: model_scale", model_size_scale)
+
+if use_mixed:
+  tf.keras.mixed_precision.set_global_policy('mixed_float16')
+ext_kwargs = {}
+if use_fp8:
+  pass
+#  ext_kwargs["is_last"] = True
+
+DenseLayer = dense_fp8.DenseFp8 if use_fp8 else layers.Dense
+
+class DotProductAttention(tf.keras.Model):
+  """Attention operation in Transformer layer"""
+
+  def __init__(
+      self,
+      num_attention_heads: int,
+      kv_channels: int,
+      attention_dropout: float,
+  ):
+    super().__init__()
+    self.projection_size = kv_channels * num_attention_heads
+    self.hidden_size_per_attention_head = float(kv_channels)
+    self.norm_factor = tf.math.sqrt(self.hidden_size_per_attention_head)
+    self.dropout = layers.Dropout(attention_dropout)
+    if self.dropout.dtype_policy.name == "mixed_float16":
+      self.norm_factor = tf.cast(self.norm_factor, dtype=tf.float16)
+
+  def masked_softmax(
+      self,
+      inp: tf.Tensor,
+      mask: Optional[tf.Tensor],
+  ) -> tf.Tensor:
+    if mask is not None:
+      inp = tf.where(mask, -10000.0, inp)
+    return tf.nn.softmax(inp, axis=-1)
+
+  def call(
+      self,
+      query: tf.Tensor,
+      key: tf.Tensor,
+      value: tf.Tensor,
+      attention_mask: Optional[tf.Tensor] = None,
+  ) -> tf.Tensor:
+    b = query.shape[0]
+    np = query.shape[2]
+    sq = query.shape[1]
+    sk = key.shape[1]
+    hn = value.shape[3]
+
+    # [b, sq, np, bn] -> [b, np, sq, bn]
+    query = tf.transpose(query, perm=(0, 2, 1, 3))
+    key = tf.transpose(key, perm=(0, 2, 3, 1))
+    query = tf.reshape(query, shape=(b * np, sq, hn))
+    key = tf.reshape(key, shape=(b * np, hn, sk))
+
+    bmm1 = tf.matmul(query, key) / self.norm_factor
+
+    # change view to [b, np, sq, sk]
+    attention_scores = tf.reshape(bmm1, shape=(b, np, sq, sk))
+
+    attention_probs = self.masked_softmax(attention_scores, None)  # attention_mask)
+
+    attention_probs = self.dropout(attention_probs)
+
+    # change view [sk, b * np, hn]
+    # value = tf.reshape(value, shape=(sk, b * np, hn))
+    value = tf.reshape(
+        tf.transpose(value, perm=(0, 2, 1, 3)), shape=(b * np, sk, hn)
+    )
+
+    # change view [b * np, sq, sk]
+    attention_probs = tf.reshape(attention_probs, shape=(b * np, sq, sk))
+
+    context = tf.matmul(attention_probs, value)
+
+    # change view to [b*np, sq, hn] - >[b, sq, np * hn]
+    context = tf.reshape(context, shape=(b, np, sq, hn))
+    context = tf.transpose(context, perm=(0, 2, 1, 3))
+    context = tf.reshape(context, shape=(b, sq, np * hn))
+    return context
+
+class BasicMLP(tf.keras.Model):
+  """Feed-forward network in Transformer layer"""
+
+  def __init__(
+      self,
+      hidden_size: int,
+      ffn_hidden_size: int,
+  ):
+    super().__init__()
+
+    self.linear1 = DenseLayer(units=ffn_hidden_size, use_bias=True, **ext_kwargs)
+    self.linear2 = DenseLayer(units=hidden_size, use_bias=True, **ext_kwargs)
+
+  def call(self, x: tf.Tensor) -> tf.Tensor:
+    x = self.linear1(x)
+    x = tf.nn.gelu(x, approximate=True)
+    x = self.linear2(x)
+    return x
+
+
+class BasicTransformer(tf.keras.Model):
+  def __init__(
+      self,
+      hidden_size: int,
+      ffn_hidden_size: int,
+      num_attention_heads: int,
+      layernorm_eps: int = 1e-5,
+      attention_dropout: float = 0.1,
+      hidden_dropout: float = 0.1,
+  ):
+    super().__init__()
+    self.num_attention_heads = num_attention_heads
+    self.kv_channels = hidden_size // num_attention_heads
+    self.ln1 = layers.LayerNormalization(epsilon=layernorm_eps)
+
+    self.qkv_projection = DenseLayer(
+        units=3 * hidden_size,
+        use_bias=True,
+        **ext_kwargs,
+    )
+
+    self.attention = DotProductAttention(
+        num_attention_heads=num_attention_heads,
+        kv_channels=self.kv_channels,
+        attention_dropout=attention_dropout,
+    )
+
+    self.projection = DenseLayer(units=hidden_size, use_bias=False, **ext_kwargs)
+    self.projection2 = DenseLayer(units=3*hidden_size, use_bias=False, **ext_kwargs)
+
+    self.dropout = layers.Dropout(hidden_dropout)
+    self.ln2 = layers.LayerNormalization(epsilon=layernorm_eps)
+    self.mlp = BasicMLP(
+        hidden_size=hidden_size,
+        ffn_hidden_size=ffn_hidden_size,
+    )
+
+  def call(
+      self,
+      x: tf.Tensor,
+      #      attention_mask: tf.Tensor,
+  ) -> tf.Tensor:
+
+    x = self.ln1(x)
+    x = self.projection2(x)
+    x = self.projection(x)
+    return x
+#    res = x
+#    x = self.ln1(x)
+#
+#    # Fused QKV projection
+#    qkv = self.qkv_projection(x)
+#    qkv_shape = qkv.shape
+#    qkv = tf.reshape(
+#        qkv,
+#        shape=(
+#            qkv_shape[0],
+#            qkv_shape[1],
+#            self.num_attention_heads,
+#            3 * self.kv_channels,
+#        ),
+#    )
+#    q, k, v = tf.split(qkv, 3, axis=3)
+#
+#    attention_mask = None
+#    x = self.attention(q, k, v, attention_mask)
+#    x = self.projection(x)
+#    x = self.dropout(x)
+#    x = res + x
+#    res = x
+#    x = self.ln2(x)
+#    x = self.mlp(x)
+#
+#    return x + res
+
+# Layer configuration
+hidden_size = 512 * model_size_scale
+sequence_length = 128
+batch_size = 16
+ffn_hidden_size = 16384 * model_size_scale
+num_attention_heads = 32
+dtype = tf.float16
+
+opt = tf.keras.optimizers.experimental.SGD(learning_rate=0.0000001)
+def speedometer(
+    model: tf.keras.Model,
+    x: tf.Tensor,
+    y: tf.Tensor,
+    forward_kwargs: dict = {},
+    fp8_autocast_kwargs: Optional[dict] = None,
+    timing_iters: int = 20,
+    warmup_iters: int = 20,
+) -> None:
+  """Measure average run time for a TF model
+  Performs forward and backward passes.
+  """
+  if fp8_autocast_kwargs is None:
+    fp8_autocast_kwargs = {"enabled": False}
+
+  p = tf.constant(0.0)  # Create small tensor to force GPU resync
+  for v in model.variables:
+    print("xxx variable name:", v.name)
+  for v in model.trainable_variables:
+    print("xxx trainable variable name:", v.name)
+
+  if is_train:
+    def my_loss_fun(y1,y2):
+      sf = tf.square(y1-y2)
+      return tf.reduce_mean(sf)
+    model.compile(loss=my_loss_fun, optimizer=tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.1, jit_compile=True))
+    history = model.fit(x, y, batch_size=1, epochs=20,validation_split=0.0)
+
+  else:
+    @tf.function(jit_compile=True)
+    def run_training(xx=x):
+      with tf.GradientTape(persistent=True) as tape:
+        tape.watch(xx)
+        output = model(xx, **forward_kwargs)
+        loss = tf.math.square(output - y)
+      dx, dvars = tape.gradient(loss, [xx, model.trainable_variables])
+      return output, dx, dvars
+  
+    # Warmup runs
+    for _ in range(warmup_iters):
+      out, dx, dvars = run_training()
+  
+    print((p + 1.)) # Sync the GPU
+  
+    # Timing runs
+    start = time.time()
+    for _ in range(timing_iters):
+      out, dx, dvars = run_training()
+    print((p + 1.)) # Sync the GPU
+    end = time.time()
+  
+    elapsed_time = (end - start) / timing_iters * 1000
+  
+    print(f"Mean time: {elapsed_time} ms")
+    print("xxx check dx:")
+    print(dx[0, 0], out.shape, x.shape)
+
+tf.random.set_seed(12)
+tf.keras.utils.set_random_seed(1)
+
+# Synthetic data
+x_data = tf.random.normal(shape=(batch_size, sequence_length, hidden_size),
+                          dtype=dtype)
+y_data = tf.random.normal(shape=(batch_size, sequence_length, hidden_size ),
+                          dtype=dtype)
+y_dtype = tf.float16 if use_mixed else tf.float32
+
+y_data = tf.random.normal(x_data.shape,dtype=y_dtype)
+
+basic_transformer = BasicTransformer(
+    hidden_size,
+    ffn_hidden_size,
+    num_attention_heads,
+    attention_dropout=dropout_rate,
+    hidden_dropout=dropout_rate,
+)
+
+# Run once to build the variables and make sure the model.variables doesn't
+# return None.
+test_y = basic_transformer(x_data)
+print(test_y)
+
+speedometer(
+    basic_transformer,
+    x_data,
+    y_data,
+    forward_kwargs={"training": True},
+)
+
+# Print out the summary of the model.
+basic_transformer.summary()
